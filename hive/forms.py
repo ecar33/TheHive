6159b203dabfea2f1faf60e14df0f4965a146a5b
Evan Carlile <evanc2022@gmail.com>
2024-12-18 20:22:49 -0600

    Placeholder text for form fields

--- hive/forms.py
+++ hive/forms.py
@@ -3,6 +3,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 
 class PostMessageForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 20)])
-    body = TextAreaField('Message', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": 'Enter your name...'})
+    body = TextAreaField('Message', validators=[DataRequired()], render_kw={"placeholder": 'Enter a message...'})
     submit = SubmitField()
